20ae8b828e8c633ef80a2e67bb9cff93913723dc
Ulises Tobar <ulises.se.tobar@gmail.com>
2026-03-02 00:03:30 +0000

    feat(restaurant): :sparkles: booking api

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Menu
+from .models import Menu, Booking
 
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ['id', 'Title', 'Price', 'Inventory']
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import Menu
-from .serializers import MenuSerializer
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 
 # Create your views here.
@@ -17,3 +17,8 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
